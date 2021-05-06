17aa527c4820695307e420cc31b52ad8e5876a7a
wangbin <18695686631@163.com>
2021-05-06 20:13:53 +0800

    update

--- .vuepress/siderbar.js
+++ .vuepress/siderbar.js
@@ -10,9 +10,7 @@ const OTHER_PATH = '/blogs/other'
 
 
 module.exports = {
-  [JAVASCRIPT_PATH]: [
-    [createSideBarConfig('JS基础', JAVASCRIPT_PATH)]
-  ],
+  [JAVASCRIPT_PATH]: [createSideBarConfig('js基础', JAVASCRIPT_PATH)],
   [CSS_PATH]: [createSideBarConfig('CSS基础', CSS_PATH)],
   [PERFORM_PATH]: [createSideBarConfig('前端性能相关', PERFORM_PATH)],
   [ALGORITHM_PATH]: [createSideBarConfig('算法', ALGORITHM_PATH)],
